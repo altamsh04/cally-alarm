a8ac5c8eb77ca44577d6cfef50b97d54dc5bb6cc
altamsh04 <bairagdaraltamsh@gmail.com>
2025-07-04 01:31:02 +0530

    Update files for arranging the wake up apis

---
+++ api/wake-call.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+import twilio from 'twilio';
+
+export default async function handler(req, res) {
+  const client = twilio(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+
+  try {
+    const call = await client.calls.create({
+      twiml: '<Response><Say>Good morning! Wake up! This is your scheduled call.</Say></Response>',
+      to: process.env.MY_PHONE_NUMBER,
+      from: process.env.TWILIO_PHONE_NUMBER
+    });
+
+    res.status(200).json({ message: 'Call placed', sid: call.sid });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
